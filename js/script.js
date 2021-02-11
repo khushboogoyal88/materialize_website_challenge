9964c87efca08bee81f0ad922447f571247ef85d
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-02-11 23:52:47 +0200

    NewsletterSubscription/ Form validation, Pop up for thanks and email id saved to local storage

--- js/script.js
+++ js/script.js
@@ -20,3 +20,25 @@ counters.forEach(counter=>{
     
 })
 
+const form = document.getElementById('newsletter-form');
+const email = document.getElementById('email');
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let visitor_email = email.value;
+    // Checks if array already exists, if not sets up an array of messages
+    let emailArr = JSON.parse(localStorage.getItem('emailArr')) || [];
+    emailArr.push(visitor_email)
+    // Adds email address to the local storage
+    localStorage.setItem('emailArr', JSON.stringify(emailArr));
+    // Clears fields after submission.
+    document.getElementById('email').value='';
+    
+    // Work around to make jquery open the modal, after form is submitted.
+    jQuery(document).ready(function () {
+        jQuery('#modal1').modal();
+        jQuery(document).ready(function () {
+            jQuery('#modal1').modal('open');
+        });
+    });
+});
+
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Task: Creating a responsive webiste as per the design.
+
+### Stack used HTML/CSS/JS/jQuery
+
+### Framework used: Materialize, Google fonts and Font awesome icons.
+
+### Tasks achieved:
+
+- Parallax effect on showcase image.
+- Animated Incrementing Numbers to show clients, employees and users.
+- Pop up after newsletter is subscribed.
+
+
+### Other animations and features:
+
+- Basic form validation to check the email id
+- The email address is then saved to Local Storagein an array of emailIDs.
+- Fade-in effect of main text at landing
+- Mobile Menu
+- Drop down Menu on hover for Language and Contact Us
+- Sticky Navbar
+- Hover effect on cards.
+
+
+
