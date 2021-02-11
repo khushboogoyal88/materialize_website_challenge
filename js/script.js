1fadd5abe711b519b5b8b49370ba69991ef37e2f
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-02-11 14:17:54 +0200

    favicon/Favicon added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-$(document).ready(function () {
-    $('.dropdown-button').dropdown({
-        constrainWidth: false,
-        hover: true,
-        belowOrigin: true,
-        alignment: 'left'
-    });
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter=>{
+    counter.innerText = '0'
     
-    // JAVASCRIPT START HERE //
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText;
+        
+        const increment = target/200;
+        
+        if(c < target){
+            counter.innerText = `${Math.ceil(c+ increment)}`;
+            setTimeout(updateCounter, 1);
+        }else{
+            counter.innerText = target;
+        }
+    }
+    updateCounter()
     
-    $('.button-collapse').sideNav();
-    
-    // INIT SELECT LIST
-    $('select').material_select();
-});
+})
+
